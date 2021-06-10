51f834ed56cc4ad18ea952ed79f2f3df5b92415c
peter <luzescura@gmail.com>
2021-06-10 20:26:27 +0100

    Update to button text

--- app/src/main/java/com/example/diceeroller/MainActivity.kt
+++ app/src/main/java/com/example/diceeroller/MainActivity.kt
@@ -2,11 +2,15 @@ package com.example.diceeroller
 
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var diceImage: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -16,11 +20,21 @@ class MainActivity : AppCompatActivity() {
         rollButton.setOnClickListener {
             rollDice()
         }
+        // initialising the dice image variable
+        diceImage = findViewById(R.id.dice_image)
     }
 
     private fun rollDice() {
         val randomInt = Random.nextInt(6)+1
-        val diceText : TextView = findViewById(R.id.dice_text)
-        diceText.text = randomInt.toString()
+
+        val drawableResource = when (randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        diceImage.setImageResource(drawableResource)
     }
 }
